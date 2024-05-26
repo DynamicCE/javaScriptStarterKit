84494a5b3ecf16f5f5c548709160a8b07a9b4fe4
Adınız <erkanmurat0632@gmail.com>
2024-05-26 18:02:28 +0300

    new

---
+++ src/oop/index.js
@@ -0,0 +1,9 @@
+class Customer{
+    constructor(id,customerNumber){
+        this.id = id
+        this.customerNumber = customerNumber        
+    }
+}
+
+let customer = new Customer(1,"12345")
+console.log(customer.id)
